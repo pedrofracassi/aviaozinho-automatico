13d2e95b708366f5f14b77d556920ed82115f466
Pedro Fracassi <pedroluizfracassi@gmail.com>
2017-10-03 22:12:22 -0300

    Hell yea

--- public/preview.js
+++ public/preview.js
@@ -1,42 +1,24 @@
-document.oninput = window.onresize = function(thing) {
-  if (thing.srcElement.id == 'field-message') {
-    update(thing.srcElement.value);
-  }
+window.onload = document.oninput = window.onresize = function(thing) {
+  update();
 }
 
-var gradients = [
-  ["#007991", "#78ffd6"],
-  ["#56CCF2", "#2F80ED"],
-  ["#F2994A", "#F2C94C"],
-  ["#4AC29A", "#BDFFF3"],
-  ["#B2FEFA", "#0ED2F7"],
-  ["#D66D75", "#E29587"],
-  ["#20002c", "#cbb4d4"],
-  ["#C33764", "#1D2671"],
-  ["#34e89e", "#0f3443"],
-  ["#6190E8", "#A7BFE8"],
-  ["#D38312", "#A83279"],
-  ["#00c6ff", "#0072ff"]
-]
-
-var gradient = gradients[Math.floor(Math.random() * gradients.length)];
-
-function update(text) {
-    document.getElementById('previewCanvas').height = document.getElementById('previewCanvas').width;
-    gradient = gradients[Math.floor(Math.random() * gradients.length)];
-    const canvas = document.getElementById('previewCanvas');
-    const img = document.getElementById('logo');
-    const ctx = canvas.getContext('2d');
-    var grd = ctx.createLinearGradient(0,0, canvas.width, canvas.height);
-    grd.addColorStop(0, gradient[0]);
-    grd.addColorStop(1, gradient[1]);
-    ctx.fillStyle = grd;
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-    ctx.fillStyle = "white";
-    paint_centered_wrap(canvas, 0, 0, canvas.width, canvas.height, text, 48, 2);
-    var downsacle = (canvas.width*8)/1080;
-    var logoX = (canvas.width / 2) - (img.width / downsacle / 2);
-    ctx.drawImage(img, logoX, 20, img.width / downsacle, img.height / downsacle);
+function update(text, gradient) {
+  var text = document.getElementById('field-message').value;
+  var gradient = gradients[window.currentGradient];
+  document.getElementById('previewCanvas').height = document.getElementById('previewCanvas').width;
+  const canvas = document.getElementById('previewCanvas');
+  const img = document.getElementById('logo');
+  const ctx = canvas.getContext('2d');
+  var grd = ctx.createLinearGradient(0,0, canvas.width, canvas.height);
+  grd.addColorStop(0, gradient[0]);
+  grd.addColorStop(1, gradient[1]);
+  ctx.fillStyle = grd;
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+  ctx.fillStyle = "white";
+  paint_centered_wrap(canvas, 0, 0, canvas.width, canvas.height, text, 48, 2);
+  var downsacle = (canvas.width*8)/1080;
+  var logoX = (canvas.width / 2) - (img.width / downsacle / 2);
+  ctx.drawImage(img, logoX, 20, img.width / downsacle, img.height / downsacle);
 }
 
 paint_centered_wrap = function(canvas, x, y, w, h, text, fh, spl) {
